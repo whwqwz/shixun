e6f9b5d89dff823c873f3800d82e57f7b8d4845d
whwqwz <43991326+whwqwz@users.noreply.github.com>
2018-10-09 06:58:00 -0500

    Add files via upload

---
+++ js/last.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.flexslider').flexslider();
+	$(".box").hover(function(){/*复合事件*/
+		$(this).find('span').addClass('animated fadeInDown');/*添加向下淡入的动画效果*/
+		$(this).find('p').addClass('animated fadeInUp');/*添加向上淡入的动画效果*/
+	},
+		function(){
+			$(this).find('span').removeClass('animated fadeInDown');/*移除向下淡入的动画效果*/
+			$(this).find('p').removeClass('animated fadeInUp');/*移除向上淡入的动画效果*/
+		}
+	)
+})
